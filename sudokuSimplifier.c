abc5ab91cca20e11c1106f19bceb3cd22d280a9f
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2021-07-24 18:14:56 -0300

    statistics facilities

--- sudokuSimplifier.c
+++ sudokuSimplifier.c
@@ -55,7 +55,7 @@ int printCube(int cube[HEIGHT][LINES][COLUMNS])
 unsigned int howMuchNumbersInALine(int cube[LINES][COLUMNS], unsigned int line)
 {
 	unsigned int tot = 0, col = 0;
-	for(tot = 0, col = 0; col <  COLUMNS; col++) if(cube[line][col]) tot++;
+	for(tot = 0, col = 0; col <  COLUMNS; col++) if(cube[line][col] != EMPTY) tot++;
 
 	return(tot);
 }
@@ -63,12 +63,14 @@ unsigned int howMuchNumbersInALine(int cube[LINES][COLUMNS], unsigned int line)
 unsigned int howMuchNumbersInAColumn(int cube[LINES][COLUMNS], unsigned int col)
 {
 	unsigned int tot = 0, line = 0;
-	for(tot = 0, line = 0; line <  LINES; line++) if(cube[line][col]) tot++;
+	for(tot = 0, line = 0; line <  LINES; line++) if(cube[line][col] != EMPTY) tot++;
 
 	return(tot);
 }
 
-/* Square Ids:
+/* only for 3x3 squares...
+
+   Square Ids:
 
       |       |
    1  |   2   |   3
@@ -81,10 +83,79 @@ unsigned int howMuchNumbersInAColumn(int cube[LINES][COLUMNS], unsigned int col)
       |       |    
    7  |   8   |   9  
       |       |      
+
+00 01 02 | 03 04 05 | 06 07 08
+10 11 12 | 13 14 15 | 16 17 18
+20 21 22 | 23 24 25 | 26 27 28
+---------+----------+---------
+30 31 32 | 33 34 35 | 36 37 38
+40 41 42 | 43 44 45 | 46 47 48
+50 51 52 | 53 54 55 | 56 57 58
+---------+----------+---------
+60 61 62 | 63 64 65 | 66 67 68
+70 71 72 | 73 74 75 | 76 77 78
+80 81 82 | 83 84 85 | 86 87 88
 */
-unsigned int howMuchNumbersInASquare(int cube[][LINES][COLUMNS], unsigned int square)
+int howMuchNumbersInASquare(int cube[LINES][COLUMNS], unsigned int square)
 {
-	return(0);
+	#define DEFAULT_SUDOKU_SQUARE (9)
+	unsigned int i = 0, tot = 0;
+	int *pelem[DEFAULT_SUDOKU_SQUARE] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+
+	switch(square){ // SHAME!! SHAME!! SHAME!! ... but works, and fast ...
+		case 1:
+			pelem[0] = &cube[0][0]; pelem[1] = &cube[0][1]; pelem[2] = &cube[0][2];
+			pelem[3] = &cube[1][0]; pelem[4] = &cube[1][1]; pelem[5] = &cube[1][2];
+			pelem[6] = &cube[2][0]; pelem[7] = &cube[2][1]; pelem[8] = &cube[2][2];
+			break;
+		case 2:
+			pelem[0] = &cube[0][3]; pelem[1] = &cube[0][4]; pelem[2] = &cube[0][5];
+			pelem[3] = &cube[1][3]; pelem[4] = &cube[1][4]; pelem[5] = &cube[1][5];
+			pelem[6] = &cube[2][3]; pelem[7] = &cube[2][4]; pelem[8] = &cube[2][5];
+			break;
+		case 3:
+			pelem[0] = &cube[0][6]; pelem[1] = &cube[0][7]; pelem[2] = &cube[0][8];
+			pelem[3] = &cube[1][6]; pelem[4] = &cube[1][7]; pelem[5] = &cube[1][8];
+			pelem[6] = &cube[2][6]; pelem[7] = &cube[2][7]; pelem[8] = &cube[2][8];
+			break;
+		case 4:
+			pelem[0] = &cube[3][0]; pelem[1] = &cube[3][1]; pelem[2] = &cube[3][2];
+			pelem[3] = &cube[4][0]; pelem[4] = &cube[4][1]; pelem[5] = &cube[4][2];
+			pelem[6] = &cube[5][0]; pelem[7] = &cube[5][1]; pelem[8] = &cube[5][2];
+			break;
+		case 5:
+			pelem[0] = &cube[3][3]; pelem[1] = &cube[3][4]; pelem[2] = &cube[3][5];
+			pelem[3] = &cube[4][3]; pelem[4] = &cube[4][4]; pelem[5] = &cube[4][5];
+			pelem[6] = &cube[5][3]; pelem[7] = &cube[5][4]; pelem[8] = &cube[5][5];
+			break;
+		case 6:
+			pelem[0] = &cube[3][6]; pelem[1] = &cube[3][7]; pelem[2] = &cube[3][8];
+			pelem[3] = &cube[4][6]; pelem[4] = &cube[4][7]; pelem[5] = &cube[4][8];
+			pelem[6] = &cube[5][6]; pelem[7] = &cube[5][7]; pelem[8] = &cube[5][8];
+			break;
+		case 7:
+			pelem[0] = &cube[6][0]; pelem[1] = &cube[6][1]; pelem[2] = &cube[6][2];
+			pelem[3] = &cube[7][0]; pelem[4] = &cube[7][1]; pelem[5] = &cube[7][2];
+			pelem[6] = &cube[8][0]; pelem[7] = &cube[8][1]; pelem[8] = &cube[8][2];
+			break;
+		case 8:
+			pelem[0] = &cube[6][3]; pelem[1] = &cube[6][4]; pelem[2] = &cube[6][5];
+			pelem[3] = &cube[7][3]; pelem[4] = &cube[7][4]; pelem[5] = &cube[7][5];
+			pelem[6] = &cube[8][3]; pelem[7] = &cube[8][4]; pelem[8] = &cube[8][5];
+			break;
+		case 9:
+			pelem[0] = &cube[6][6]; pelem[1] = &cube[6][7]; pelem[2] = &cube[6][8];
+			pelem[3] = &cube[7][6]; pelem[4] = &cube[7][7]; pelem[5] = &cube[7][8];
+			pelem[6] = &cube[8][6]; pelem[7] = &cube[8][7]; pelem[8] = &cube[8][8];
+			break;
+		default:
+			return(-1);
+			break;
+	}
+
+	for(i = 0, tot = 0; i < DEFAULT_SUDOKU_SQUARE; i++) if(*pelem[i] != EMPTY) tot++;
+
+	return(tot);
 }
 
 int cleanFixedNumbers(int cube[][LINES][COLUMNS])
